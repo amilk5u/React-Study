1449891f5f768e7eb5cc583ef75165a41e2e88a1
amilk5u <amilk5u@naver.com>
2022-03-20 15:04:03 +0900

    second commit2

--- day03/src/components/Test6.js
+++ day03/src/components/Test6.js
@@ -1,11 +1,32 @@
+import { useState } from 'react'
 
 const Test6 = () => {
+   const [userId, setUserId] = useState('')
+   const [userPw, setUserPw] = useState('')
+
+   const changeInput1 = (e) => {
+      // setUserId(e.target.value)
+      const { value } = e.target
+      setUserId(value)
+   }
+   const changeInput2 = (e) => {
+      // setUserPw(e.target.value)
+      const { value } = e.target
+      setUserPw(value)
+   }
+
+   const onReset = () => {
+      setUserId('')
+      setUserPw('')
+   }
+
    return (
       <div>
-         <input type="text"/>
-         <input type="text"/>
+         <input type="text" onChange={changeInput1} value={userId}/>
+         <input type="text" onChange={changeInput2} value={userPw} />
+         <button onClick={onReset}>초기화</button>
          <h2>
-            id : {} / pw : {}
+            id : {userId} / pw : {userPw}
          </h2>
       </div>
    );
