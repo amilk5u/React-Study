a57b2ffd5f69ab7c2b00320fb36ea934b92b0ea4
amilk5u <amilk5u@naver.com>
2022-03-31 20:36:45 +0900

    commit

--- day03/src/App.js
+++ day03/src/App.js
@@ -1,9 +1,9 @@
-import Test3 from './components/Test3';
+import Test6 from './components/Test6';
 
 const App = () => {
    return (
       <div>
-         <Test3 />
+         <Test6 />
       </div >
    );
 };
