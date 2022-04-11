b6766e20b480faa18f787d2df0f90d39d621e68d
amilk5u <amilk5u@naver.com>
2022-04-11 10:01:21 +0900

    2022.04.11 first commit

--- day06/src/App.js
+++ day06/src/App.js
@@ -1,11 +1,15 @@
-import Test1 from './components/Test1'
+import React from 'react';
+import Test7 from './components/Test7';
+import './assets/css/reset.css'
+import Customer from './sample1/Customer';
 
 const App = () => {
-   return (
-      <div>
-         <Test1 />
-      </div>
-   );
+  return (
+    <div>
+      {/* <Test7 /> */}
+      <Customer />
+    </div>
+  );
 };
 
 export default App;
